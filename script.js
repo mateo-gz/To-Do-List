420f071c1227d5d9a63dac1a8304e40905824c17
Mateo Gomez <mr.mateo.oficial@gmail.com>
2025-04-01 19:42:10 -0600

    To-Do List terminada completamente

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const tareasForm = document.querySelector(".tareas-form");
     const abrirContenedorTareas = document.getElementById("abrir-contenedor");
     const abrirContenedorTareasCompletadas = document.getElementById("abrir-contenedor-tareas-completadas");
+    const abrirVentanaModal = document.querySelector(".abrir-ventana-modal-input");
+    const ventanaModal = document.querySelector(".ventana-modal");
+    const fondoVentanaModal = document.querySelector(".fondo-ventana-modal");
+    const cerrarModal = document.querySelector(".cerrar-ventana-modal");
+    const horaTarea = document.querySelector(".hora");
 
     // Mostrar tareas pendientes
     const mostrarTareas = ()=> {
@@ -18,7 +23,8 @@ document.addEventListener("DOMContentLoaded", () => {
             tareaDiv.classList.add("tarea-div");
             tareaDiv.innerHTML = `
                 <input type="checkbox" class="tarea-checkbox" data-id="${tarea.id}">
-                ${tarea.nombre} 
+                <div>${tarea.nombre}</div>
+                <div>${tarea.hora}</div>
                 <button class="btn-remove" onclick="eliminarTarea(${tarea.id})">X</button> 
             `;
             contenedorTareas.appendChild(tareaDiv);
@@ -43,8 +49,10 @@ document.addEventListener("DOMContentLoaded", () => {
         tareasCompletadas.forEach((tarea) => {
             let tareaDiv = document.createElement("div");
             tareaDiv.classList.add("tarea-div");
+
             tareaDiv.innerHTML = `
-                ${tarea.nombre} 
+                <div>${tarea.nombre}</div>
+                <div>${tarea.hora}</div>
                 <button class="btn-remove" onclick="eliminarTareaCompletada(${tarea.id})">X</button>
             `;
             contenedorTareasCompletadas.appendChild(tareaDiv);
@@ -95,12 +103,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
         let nombreNuevaTarea = nombreTarea.value.trim();
 
-        if (nombreNuevaTarea !== "") {
+        if (nombreNuevaTarea !== "" && horaTarea.value !== "") {
             let tareas = JSON.parse(localStorage.getItem("tareas")) || [];
 
             let nuevaTarea = {
                 id: Date.now(),
-                nombre: nombreNuevaTarea
+                nombre: nombreNuevaTarea,
+                hora: horaTarea.value
             };
 
             tareas.push(nuevaTarea);
@@ -108,9 +117,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
             nombreTarea.value = ""; // Limpiar input
             mostrarTareas(); // Volver a renderizar
+            ventanaModal.style.display = "none";
+            fondoVentanaModal.style.display = "none";
         } else {
-            alert("El nombre de la tarea está vacío");
+            alert("Coloca bien los campos");
         }
+
     });
 
     // Mostrar tareas al cargar
@@ -119,10 +131,40 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Mostrar u ocultar contenedores según checkboxes
     abrirContenedorTareas.addEventListener("change", () => {
-        contenedorTareas.style.display = abrirContenedorTareas.checked ? "flex" : "none";
+        if (abrirContenedorTareas.checked) {
+            contenedorTareas.style.height = "100%";
+            contenedorTareas.style.padding = "10px";
+        } else {
+            contenedorTareas.style.height = "0%";
+            contenedorTareas.style.padding = "0px";
+        }
     });
+    
 
     abrirContenedorTareasCompletadas.addEventListener("change", () => {
-        contenedorTareasCompletadas.style.display = abrirContenedorTareasCompletadas.checked ? "flex" : "none";
+        if (abrirContenedorTareasCompletadas.checked) {
+            contenedorTareasCompletadas.style.height = "100%";
+            contenedorTareasCompletadas.style.padding = "10px";
+        } else {
+            contenedorTareasCompletadas.style.height = "0%";
+            contenedorTareasCompletadas.style.padding = "0px";
+        }
     });
+
+    abrirVentanaModal.addEventListener("change", () => {
+        if (abrirVentanaModal.checked) {
+            ventanaModal.style.display = "flex";
+            fondoVentanaModal.style.display = "block"
+        } else {
+            ventanaModal.style.display = "none";
+            fondoVentanaModal.style.display = "none"
+        }
+    });
+
+    cerrarModal.addEventListener("click", ()=>{
+            fondoVentanaModal.style.display = "none"
+        ventanaModal.style.display = "none";
+    })
+
+    
 });
